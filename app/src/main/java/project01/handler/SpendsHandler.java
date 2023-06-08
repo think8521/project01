10d007f544d46719f4fe61daf153fb2ab4910a95
think8521 <think8521@naver.com>
2023-06-09 01:47:45 +0900

    .

--- app/src/main/java/project01/handler/SpendsHandler.java
+++ app/src/main/java/project01/handler/SpendsHandler.java
@@ -2,45 +2,93 @@ package project01.handler;
 
 import util.Prompt;
 
+import project01.vo.Spend;
+
 public class SpendsHandler {
 
   static final int MAX_SIZE = 100;
   static int userId = 1;
   static int length = 0;
-  static int[] no = new int[MAX_SIZE];
-  static String[] spend = new String[MAX_SIZE];
-  static String[] price = new String[MAX_SIZE];
-  static char[] dailyNecessity = new char[MAX_SIZE];
-  static String[] YesOrNo = new String[MAX_SIZE];
+  static Spend[] spends = new Spend[MAX_SIZE];
+  // static int[] no = new int[MAX_SIZE];
+  // static String[] spend = new String[MAX_SIZE];
+  // static String[] price = new String[MAX_SIZE];
+  // static char[] dailyNecessity = new char[MAX_SIZE];
+  // static String[] YesOrNo = new String[MAX_SIZE];
 
   static final char Yes = 'Y';
   static final char No = 'N';
 
   public static void inputSpend() {
+    if (!available()) {
+      System.out.println("더이상 입력할 수 없습니다!");
+      return;
+    }
 
-    spend[length] = Prompt.inputString("지출명? ");
+    Spend s = new Spend();
+    s.spend = Prompt.inputString("지출명? ");
+    s.price = Prompt.inputString("금액? ");
+    s.dailyNecessity = inputDaily((char) 0);
 
-    price[length] = Prompt.inputString("금액? ");
+    s.no = userId++;
+    spends[length++] = s;
+  }
 
-    loop: while (true) {
-
-      String menuNo = Prompt.inputString("생필품 여부:\n" + "  1. Yes\n" + "  2. No\n" + "> ");
-
-      switch (menuNo) {
-        case "1":
-          dailyNecessity[length] = Yes;
-          YesOrNo[length] = "Yes";
-          break loop;
-        case "2":
-          dailyNecessity[length] = No;
-          YesOrNo[length] = "No";
-          break loop;
-        default:
-          System.out.println("무효한 번호입니다.");
+  public static void viewSpend() {
+    String spendNo = Prompt.inputString("번호? ");
+    // 입력 받은 번호를 가지고 배열에서 해당 회원을 찾아야 한다.
+    for (int i = 0; i < length; i++) {
+      Spend s = spends[i];
+      if (s.no == Integer.parseInt(spendNo)) {
+        // i 번째 항목에 저장된 회원 정보 출력
+        System.out.printf("지출명: %s\n", s.spend);
+        System.out.printf("금액: %s\n", s.price);
+        System.out.printf("생필품 여부: %s\n", toDailyString(s.dailyNecessity));
+        return;
       }
     }
-    no[length] = userId++;
-    length++;
+    System.out.println("해당 번호의 지출내역이 없습니다!");
+  }
+
+  public static void updateSpend() {
+    String spendNo = Prompt.inputString("번호? ");
+    // 입력 받은 번호를 가지고 배열에서 해당 회원을 찾아야 한다.
+    for (int i = 0; i < length; i++) {
+      Spend s = spends[i];
+      if (s.no == Integer.parseInt(spendNo)) {
+        // i 번째 항목에 저장된 회원 정보 출력
+        System.out.printf("지출명(%s)? ", s.spend);
+        s.spend = Prompt.inputString("");
+        System.out.printf("금액(%s)? ", s.price);
+        s.price = Prompt.inputString("");
+        s.dailyNecessity = inputDaily(s.dailyNecessity);
+        return;
+      }
+    }
+    System.out.println("해당 번호의 지출내역이 없습니다!");
+  }
+
+  public static void deleteSpend() {
+    int spendNo = Prompt.inputInt("번호? ");
+    int deletedIndex = indexOf(spendNo);
+    if (deletedIndex == -1) {
+      System.out.println("해당 번호의 지출내역이 없습니다!");
+      return;
+    }
+    for (int i = deletedIndex; i < length - 1; i++) {
+      spends[i] = spends[i + 1];
+    }
+    spends[--length] = null;
+  }
+
+  private static int indexOf(int spendNo) {
+    for (int i = 0; i < length; i++) {
+      Spend s = spends[i];
+      if (s.no == spendNo) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   public static void printSpends() {
@@ -49,11 +97,48 @@ public class SpendsHandler {
     System.out.println("--------------------------------");
 
     for (int i = 0; i < length; i++) {
-      System.out.printf("%d, %s, %s원, %s\n", no[i], spend[i], price[i], YesOrNo[i]);
+      Spend s = spends[i];
+      System.out.printf("%d, %s, %s원, %s\n", s.no, s.spend, s.price, toDailyString(s.dailyNecessity));
+    }
+    int total = 0;
+    for (int i = 0; i < length; i++) {
+      Spend s = spends[i];
+      total += Integer.parseInt(s.price);
+    }
+    String totals = Integer.toString(total);
+    System.out.printf("지출 총액: %s원\n", totals);
+  }
+
+  private static char inputDaily(char dailyNecessity) {
+    String label;
+    if (dailyNecessity == 0) {
+      label = "생필품 여부?\n";
+    } else {
+      label = String.format("생필품 여부(%s)?\n", toDailyString(dailyNecessity));
+    }
+
+    loop: while (true) {
+      String menuNo = Prompt.inputString(label +
+          "  1. Yes\n" +
+          "  2. No\n" +
+          "> ");
+
+      switch (menuNo) {
+        case "1":
+          return Yes;
+        case "2":
+          return No;
+        default:
+          System.out.println("무효한 번호입니다.");
+      }
     }
   }
 
-  public static boolean available() {
+  public static String toDailyString(char Daily) {
+    return Daily == 'Y' ? "Yes" : "No";
+  }
+
+  private static boolean available() {
     return length < MAX_SIZE;
   }
 }
--- app/src/main/java/util/Prompt.java
+++ app/src/main/java/util/Prompt.java
@@ -6,18 +6,22 @@ public class Prompt {
 
   static Scanner scanner = new Scanner(System.in);
 
-  public static boolean promptContinue() {
-    System.out.print("계속 하시겠습니까?(Y/n) ");
-    String response = scanner.nextLine();
-    if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
-      return false;
-    }
-    return true;
-  }
+  // public static boolean promptContinue() {
+  //   System.out.print("계속 하시겠습니까?(Y/n) ");
+  //   String response = scanner.nextLine();
+  //   if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
   public static String inputString(String title) {
     System.out.print(title);
     return scanner.nextLine();
+  }
+
+  public static int inputInt(String title) {
+    return Integer.parseInt(inputString(title));
   }
 
   public static void close() {
